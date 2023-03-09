c2db2963ff8e664a08696bd783223893ac9d0afa
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-03-09 15:44:32 +0500

    Add checkbox

--- src/index.js
+++ src/index.js
@@ -16,14 +16,36 @@ const getItemsfromLocal = JSON.parse(localStorage.getItem('localStorageTasks'));
 const listContainer = document.querySelector('.todo-list');
 
 if (getItemsfromLocal != null) {
+  listContainer.textContent = '';
   getItemsfromLocal.forEach((anItem) => {
-    listContainer.innerHTML += `
-    <div class="item-container" id="${anItem.index}">
-      <div class="item-name" onclick="editData(${anItem.index})">${anItem.description}</div>
-      <button type="button" class="remove-btn" onclick="removeItem(${anItem.index})">Remove</button>
-    </div>
-    `;
+    // const funcParam = anItem.index
+
+    const itemContainer = document.createElement('div')
+    itemContainer.classList.add('item-container');
+    itemContainer.setAttribute('id', anItem.index);
+    
+    const itemCheck = document.createElement('input');
+    itemCheck.classList.add('item-check');
+    itemCheck.setAttribute('type', 'checkbox')
+
+    const itemName = document.createElement('div');
+    itemName.classList.add('item-name');
+    itemName.setAttribute('onclick', 'editData(anItem.index)');
+    itemName.textContent = anItem.description;
+
+    const remvButton = document.createElement('button');
+    remvButton.classList.add('remove-button');
+    remvButton.setAttribute('type', 'button');
+    remvButton.setAttribute('onclick', 'removeItem(anItem.index)');
+    remvButton.textContent = 'Remove';
+
+    itemContainer.append(itemCheck, itemName, remvButton);
+    listContainer.appendChild(itemContainer);
+    
     const newItemLocal = new Tasks(anItem.index, anItem.description, anItem.completed);
+    if (anItem.completed) {
+      itemCheck.setAttribute('checked', '')
+    }
     toDoList.push(newItemLocal);
   });
 }
